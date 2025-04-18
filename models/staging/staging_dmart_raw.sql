24e50885288b40850fac78ed123a2eb3cc9eb6a0
Chandan Vali <chandancvali5@gmail.com>
2025-04-18 11:04:20 +0000

    added fact tables, tests and macros

--- models/staging/staging_dmart_raw.sql
+++ models/staging/staging_dmart_raw.sql
@@ -5,16 +5,21 @@
 }}
 
 select
-order_id,
-upper(customer_name) as customer_name,
-category,
-sub_category,
-city,
-coalesce(try_to_date(order_date, 'dd-mm-yyyy'), try_to_date(order_date, 'mm/dd/yyyy')) as order_date,
-region,
-sales,
-discount,
-profit,
-state
+raw.order_id,
+upper(raw.customer_name) as customer_name,
+raw.category,
+raw.sub_category,
+raw.city,
+coalesce(try_to_date(raw.order_date,'dd-mm-yyyy'),
+        try_to_date(raw.order_date,'mm/dd/yyyy')) as order_date,
+raw.region,
+raw.sales,
+raw.discount,
+raw.profit,
+raw.state,
+gst.gst_code
 from 
-{{ source('raw1', 'dmart_raw') }}
+{{ source('raw1', 'dmart_raw') }} raw
+left join
+{{ ref('gst') }} gst
+on raw.state=gst.state
